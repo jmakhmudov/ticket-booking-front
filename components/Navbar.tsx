8afd116d70c53db716501219ab13ab635cae9277
jmakhmudov <makhmudov@tuta.io>
2023-09-10 14:39:48 +0500

    menubar bug fix

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,21 +1,17 @@
 "use client"
 
+import { Cross1Icon, HamburgerMenuIcon } from "@radix-ui/react-icons";
 import ModeToggle from "./ModeToggle";
-import { HamburgerMenuIcon, Cross1Icon } from "@radix-ui/react-icons";
-import { useState } from "react";
 import { Button } from "./ui/button";
+import { useState } from "react";
 
 type MenuPosition = "0" | "-100vw"
 
 const Navbar = () => {
-    const [menuPosition, setMenuPosition] = useState<MenuPosition>("-100vw");
+    const [menu, setMenu] = useState<boolean>(false);
 
-    const openMenu = () => {
-        setMenuPosition("0");
-    }
-
-    const closeMenu = () => {
-        setMenuPosition("-100vw")
+    const handleClick = () => {
+        setMenu(!menu);
     }
 
     return (
@@ -24,13 +20,13 @@ const Navbar = () => {
 
             <section className="flex gap-5 items-center">
                 <ModeToggle />
-                <Button onClick={openMenu} variant={"outline"} className="px-2 py-4">
+                <Button onClick={handleClick} variant={"outline"} className="px-2 py-4">
                     <HamburgerMenuIcon className="w-5 h-6" />
                 </Button>
             </section>
 
-            <section className={`right-[${menuPosition}] bottom-0 top-0 absolute w-72 bg-background border-l-[1px] p-5 transition-all duration-700`}>
-                <Button onClick={closeMenu} variant={"outline"} className="px-2 py-4">
+            <section className={`${menu ? "right-0" : "right-[-100vw]"} bottom-0 top-0 absolute w-72 bg-background border-l-[1px] p-5 transition-all duration-700`}>
+                <Button onClick={handleClick} variant={"outline"} className="px-2 py-4">
                     <Cross1Icon className="w-5 h-6" />
                 </Button>
             </section>
