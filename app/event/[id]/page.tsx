f6265ae05c6362dcc1bfa829b162face61cb3aae
jmakhmudov <makhmudov@tuta.io>
2023-09-16 01:43:07 +0500

    event page

--- app/event/[id]/page.tsx
+++ app/event/[id]/page.tsx
@@ -1,13 +1,50 @@
 'use client'
 
-import { useRouter } from 'next/navigation'
-import { usePathname } from 'next/navigation'
-import React from 'react'
+import Navbar from '@/components/Navbar';
+import { Badge } from '@/components/ui/badge';
+import { selectAllEvents } from '@/features/events/eventsSlice';
+import { MixIcon, PaperPlaneIcon, PersonIcon } from '@radix-ui/react-icons';
+import format from 'date-fns/format';
+import { useParams } from 'next/navigation';
+import { useSelector } from 'react-redux';
 
 const EventInfo = () => {
-    const router = usePathname()
-    console.log(router)
-    return (<p>Post: {1}</p>)
+    const router = useParams()
+    const events = useSelector(selectAllEvents);
+
+    const event = events.find(e => e.id === Number(router.id))
+
+    return (
+        <main>
+            <Navbar />
+            <section className='p-5 sm:px-10 space-y-5 '>
+                <img className="w-full h-60 object-cover rounded-sm" src={event?.thumbnail} alt={event.name} />
+
+                <section className='space-y-4'>
+                    <p className='opacity-50'>{format(new Date(event.date), 'p, dd/MM/yyyy')}</p>
+                    <h1 className="font-bold text-3xl">{event?.name}</h1>
+
+                    <div className="text-sm my-2 space-x-2">
+                        <Badge >
+                            <PaperPlaneIcon className="w-3 mr-1" />
+                            {event.place}
+                        </Badge>
+                        <Badge>
+                            <MixIcon className="w-3 mr-1" />
+                            {event.topic}
+                        </Badge>
+                        <Badge variant={Number(event.number_of_seats) < 10 ? "destructive" : "default"}>
+                            <PersonIcon className="w-3 mr-1" />
+                            {event.number_of_seats} left
+                        </Badge>
+                    </div>
+
+                    <p className='max-w-lg'>{event?.description}</p>
+                </section>
+            </section>
+
+        </main>
+    )
 }
 
 export default EventInfo;
